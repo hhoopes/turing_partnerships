90ce0c9b4a6e3c25034d5e6c3933745d0a2e189f
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-02-25 16:47:45 -0700

    add default to role

--- db/migrate/20160225215802_add_column_to_students.rb
+++ db/migrate/20160225215802_add_column_to_students.rb
@@ -2,6 +2,6 @@ class AddColumnToStudents < ActiveRecord::Migration
   def change
     add_column :students, :username, :string
     add_column :students, :password_digest, :string
-    add_column :students, :role, :integer
+    add_column :students, :role, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -44,11 +44,11 @@ ActiveRecord::Schema.define(version: 20160225215802) do
     t.string   "name"
     t.string   "image"
     t.string   "cohort"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "username"
     t.string   "password_digest"
-    t.integer  "role"
+    t.integer  "role",            default: 0
   end
 
   add_foreign_key "student_projects", "partnerships"
