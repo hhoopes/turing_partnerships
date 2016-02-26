cde419eb462d2e677b219f8c6b8525a74f9c2d1e
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-02-25 20:09:13 -0700

    student CRUD functionality for admins

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -10,20 +10,30 @@ class Admin::StudentsController < Admin::BaseController
     else
       render :new
     end
+  end
 
-    def index
-      @students = Student.all
-    end
+  def index
+    @students = Student.all
+  end
 
-    def show
-      @student = Student.find(params[:id])
-    end
+  def show
+    @student = Student.find(params[:id])
+  end
 
-    def destroy
-      @student = Student.find(params[:id])
-      @student.destroy
-      redirect_to admin_students_path
-    end
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to admin_students_path
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to admin_students_path
   end
 
   private
--- test/integration/admin_deletes_a_student_test.rb
+++ test/integration/admin_deletes_a_student_test.rb
@@ -4,18 +4,8 @@ class AdminDeletesAStudentTest < ActionDispatch::IntegrationTest
   test "admin deletes a student" do
 
     admin = Student.create(username: "heidi", password: "password", role: 1, name: "Heidi", cohort: "1511")
-    # student = Student.create(username: "adrienne", password: "password", name: "Adrienne", cohort: "1511")
+    student = Student.create(username: "adrienne", password: "password", name: "Adrienne", cohort: "1511")
     ApplicationController.any_instance.stubs(:current_user).returns(admin)
-
-    visit new_admin_student_path
-
-    fill_in "Username", with: "adrienne"
-    fill_in "Password", with: "password"
-    fill_in "Name", with: "Adrienne"
-    fill_in "Cohort", with: "1511"
-    click_on "Create Student"
-
-    student = Student.last
 
     visit admin_student_path(student)
     click_on "Delete"
