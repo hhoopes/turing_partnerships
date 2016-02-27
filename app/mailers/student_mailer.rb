4a44b7daea7d6b5e8fface1196840fb0b7abc913
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-02-27 13:43:37 -0700

    mailer settings

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -2,8 +2,6 @@ class StudentMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    require "pry"
-    binding.pry
     mail(to: @user.email, subject: "Your Account on Turing Partnerships")
   end
 end
