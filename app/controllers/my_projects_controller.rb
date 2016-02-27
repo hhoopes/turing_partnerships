a66a64f60df1ad7531507858faa3f32881e2edce
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-02-27 09:58:25 -0700

    working on partnership view

--- app/controllers/my_projects_controller.rb
+++ app/controllers/my_projects_controller.rb
@@ -7,8 +7,20 @@ class MyProjectsController < ApplicationController
 
   def create
     @student = Student.find(params[:student_id])
-    @student.my_projects.create(my_project_params)
+    my_project = @student.my_projects.create(my_project_params)
+    my_project.partnership = Partnership.create
+    new_partnership_id = Partnership.last
+    partner_students[:partnership].each do |partner|
+      student = Student.find(partner)
+      matching_project = student.my_projects.where(project_id: @student.my_projects.last.project_id)
+      matching_project.last.update(partnership_id: new_partnership_id.id)
+    end
     redirect_to student_path(@student)
+  end
+
+  def show
+    @my_project = MyProject.find(params[:id])
+    @student = Student.find(params[:student_id])
   end
 
   def destroy
@@ -21,5 +33,9 @@ class MyProjectsController < ApplicationController
 
   def my_project_params
     params.require(:project).permit(:project_id)
+  end
+
+  def partner_students
+    params.require(:project).permit(partnership: [])
   end
 end
