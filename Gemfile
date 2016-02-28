7d5f22816d787f0ce035760bffe40127047ab685
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-02-28 14:12:01 -0700

    update student model for paperclip

--- Gemfile
+++ Gemfile
@@ -32,7 +32,8 @@ gem 'rails_12factor', group: :production
 # gem 'capistrano-rails', group: :development
 
 gem 'paperclip'
-gem 'aws-sdk'
+# gem 'aws-sdk'
+gem 'aws-sdk', '< 2.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,13 @@ class Student < ActiveRecord::Base
     favicon: '16x16>',
     square: '200x200#',
     medium: '300x300>'
+  },
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
+
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
